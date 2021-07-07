82b2cc0f9578121714613dd71133ee128d5bae95
XiaHanyu <zhouyuhan_@outlook.com>
2021-07-07 17:57:32 +0800

    finish lecture 2

--- draft.go
+++ draft.go
@@ -10,9 +10,18 @@ type I interface {
 
 type Float float64
 
+type Vertex struct {
+	X, Y int
+}
+
 func (f *Float)	Change(v float64) {
 	*f = Float(v)
 }
+
+var (
+	x int = 1
+	meg string = "This"
+)
 
 
 
@@ -21,4 +30,6 @@ func main() {
 	num := Float(10.1)
 	num.Change(1.0)
 	fmt.Println(num)
+	arr := make([]int32, 0, 5)
+	fmt.Printf("%T, len = %d, cap = %d, %v", arr, len(arr), cap(arr), arr)
 }
